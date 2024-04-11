7aabc9a307e69d51d17fcb5957f0e72b0f70442b
kong0107 <kong0107@gmail.com>
2024-04-11 18:55:14 +0800

    support media collection page

--- show-sensitive.js
+++ show-sensitive.js
@@ -1,6 +1,12 @@
 setInterval(() => {
-    const target = document.querySelector('article:not([data-unblurred]) [id]:not([role="group"]) [role="button"]');
+    const target = document.querySelector('article:not([data-unblurred]) [id]:not([role="group"]) [role="button"]:not([aria-label])'); // home and single tweet
     if (! target) return;
     target.closest('article').dataset.unblurred = '';
     target.click();
 }, 1000);
+
+setInterval(() => {
+    const target = document.querySelector('section [role="listitem"] [role="button"]'); // media collection page
+    if (! target) return;
+    target.click();
+}, 100);
