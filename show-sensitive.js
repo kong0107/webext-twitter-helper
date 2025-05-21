c8d320776647bf51d190f055bbd6be4b8905526f
kong0107 <kong0107@gmail.com>
2025-05-21 22:52:17 +0800

    manifest v3

--- show-sensitive.js
+++ show-sensitive.js
@@ -1,9 +1,11 @@
 console.log('Twitter Helper');
 
-setInterval(() => {
-    document.querySelectorAll('article [id]:not([role="group"]) [role="link"] + [role="button"]').forEach(btn => btn.click());
-}, 1000);
+// setInterval(() => {
+//     document.querySelectorAll('article [id]:not([role="group"]) [role="link"] + [role="button"]').forEach(btn => btn.click());
+// }, 1000);
 
-setInterval(() => {
-    document.querySelectorAll('section [role="listitem"] [role="link"] + [role="button"]').forEach(btn => btn.click());
-}, 1000);
+// setInterval(() => {
+//     document.querySelectorAll('section [role="listitem"] [role="link"] + [role="button"]').forEach(btn => btn.click());
+// }, 1000);
+
+setInterval(() => document.querySelector('[href*="/photo/"] + button')?.click(), 100);
